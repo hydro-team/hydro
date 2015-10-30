28d2e7cabf91d78603c23a57c50f9807ed252b60
QbieShay <cislaghi.ilaria@gmail.com>
2015-10-30 15:59:13 +0100

    refactored folders

---
+++ Assets/Game/TestGesture.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestGesture : MonoBehaviour,GestureObserver {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void notify(Gesture gesture){
+
+	}
+}
