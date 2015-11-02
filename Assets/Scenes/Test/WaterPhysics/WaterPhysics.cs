32bce54e65dceed3c3b55ea74865b693e1efef75
Francesco Komauli <francesco.komauli@gmail.com>
2015-11-02 14:22:49 +0100

    Test scene for drag in a fluid

---
+++ Assets/Scenes/Test/WaterPhysics/WaterPhysics.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterPhysics : MonoBehaviour {
+
+    public float dragCoefficient;
+
+    Rigidbody body;
+
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+        var direction = -body.velocity.normalized;
+        var speed = body.velocity.magnitude;
+        var force = speed * speed * dragCoefficient;
+        body.AddForce(direction * force);
+	}
+}
