c1864106395c2ada6ca6c8854b7fa971fa9c0fc7
Francesco Komauli <francesco.komauli@gmail.com>
2015-11-02 18:17:05 +0100

    Thrust sources with incremental force that decrease with the distance from its origin

---
+++ Assets/Scenes/Test/WaterFlow/Flow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flow : MonoBehaviour {
+
+    public float strength;
+
+    void OnTriggerStay(Collider other) {
+        var body = other.gameObject.GetComponent<Rigidbody>();
+        if (body != null) {
+            var height = transform.localScale.y;
+            var flowBase = transform.position - (transform.up * 0.5f);
+            var distance = (other.transform.position - flowBase).magnitude;
+            if (distance < height) {
+                var proportion = (height - distance) / height;
+                var force = transform.up * strength * proportion;
+                body.AddForce(force);
+                var flowEnd = transform.position + (transform.up * 0.5f);
+                Debug.DrawLine(flowEnd, flowEnd + force);
+            }
+        }
+    }
+}
