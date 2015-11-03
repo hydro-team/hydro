65eca6446993ce7adbe3b3065871809e5d06f9a0
Francesco Komauli <francesco.komauli@gmail.com>
2015-11-03 11:29:48 +0100

    Flow of force decaying with time

---
+++ Assets/Scenes/Test/WaterFlow/DecayingFlow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DecayingFlow : Flow {
+
+    public float duration;
+    float remaining;
+
+    void Start() {
+        remaining = duration;
+    }
+
+	void Update () {
+        if (remaining > 0) { remaining -= Time.deltaTime; }
+	}
+
+    public override Vector3 CalculateForceFor(Rigidbody body) {
+        var proportion = remaining / duration;
+        return base.CalculateForceFor(body) * proportion;
+    }
+}
--- Assets/Scenes/Test/WaterFlow/Flow.cs
+++ Assets/Scenes/Test/WaterFlow/Flow.cs
@@ -5,19 +5,22 @@ public class Flow : MonoBehaviour {
 
     public float strength;
 
+    public virtual Vector3 CalculateForceFor(Rigidbody body) {
+        var height = transform.localScale.y;
+        var flowBase = transform.position - (transform.up * 0.5f);
+        var distance = (body.transform.position - flowBase).magnitude;
+        if (distance >= height) { return Vector3.zero; }
+        var proportion = (height - distance) / height;
+        return transform.up * strength * proportion;
+    }
+
     void OnTriggerStay(Collider other) {
         var body = other.gameObject.GetComponent<Rigidbody>();
         if (body != null) {
-            var height = transform.localScale.y;
-            var flowBase = transform.position - (transform.up * 0.5f);
-            var distance = (other.transform.position - flowBase).magnitude;
-            if (distance < height) {
-                var proportion = (height - distance) / height;
-                var force = transform.up * strength * proportion;
-                body.AddForce(force);
-                var flowEnd = transform.position + (transform.up * 0.5f);
-                Debug.DrawLine(flowEnd, flowEnd + force);
-            }
+            var force = CalculateForceFor(body);
+            body.AddForce(force);
+            var flowEnd = transform.position + (transform.up * 0.5f);
+            Debug.DrawLine(flowEnd, flowEnd + force);
         }
     }
 }
