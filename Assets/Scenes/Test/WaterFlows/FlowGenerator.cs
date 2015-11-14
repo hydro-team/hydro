2dcb779611a3dcad5e49bbb0a3a1928350220168
Francesco Komauli <francesco.komauli@gmail.com>
2015-11-14 17:43:27 +0100

    Introduced 2D water flows

--- Assets/Scenes/Test/WaterFlows/FlowGenerator.cs
+++ Assets/Scenes/Test/WaterFlows/FlowGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 using Gestures;
 using Flyweight;
 
@@ -16,15 +17,17 @@ public class FlowGenerator : MonoBehaviour {
     }
 
     void GenerateFlow(Vector2 start, Vector2 end) {
-        var rotation = Quaternion.FromToRotation(Vector3.up, end - start);
-        var cameraDistance = -camera.transform.position.z;
-        Vector2 from = camera.ScreenToWorldPoint(new Vector3(start.x, start.y, cameraDistance));
-        Vector2 to = camera.ScreenToWorldPoint(new Vector3(end.x, end.y, cameraDistance));
         var got = flows.TryRequest(flow => {
-            flow.GetComponent<DecayingFlow>().Initialize(from, to, () => {
-                flow.GetComponent<SharedObject>().ReleaseThis();
-            });
+            flow.GetComponent<Physics.Flow>().Enable(
+                from: ScreenToWorld(start),
+                to: ScreenToWorld(end),
+                onExausted: () => flow.GetComponent<SharedObject>().ReleaseThis());
         });
         if (got == null) { Debug.Log("Currently running out of flows"); }
+    }
+
+    Vector2 ScreenToWorld(Vector2 position) {
+        var cameraDistance = -camera.transform.position.z;
+        return camera.ScreenToWorldPoint(new Vector3(position.x, position.y, cameraDistance));
     }
 }
