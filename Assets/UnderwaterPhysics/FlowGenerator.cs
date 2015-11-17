804cc2f14f5c9b1f9a3ce116454254cb5f528c30
Francesco Komauli <francesco.komauli@gmail.com>
2015-11-17 16:56:56 +0100

    Flow's strength depends on the swipe duration

--- Assets/UnderwaterPhysics/FlowGenerator.cs
+++ Assets/UnderwaterPhysics/FlowGenerator.cs
@@ -7,6 +7,8 @@ namespace UnderwaterPhysics {
 
     public class FlowGenerator : MonoBehaviour {
 
+        public float maxFlowStrength;
+        public float flowDuration;
         public GameObject flowPool;
         public new Camera camera;
         public GameObject gestures;
@@ -17,14 +19,17 @@ namespace UnderwaterPhysics {
             if (flowPool.GetComponent<ObjectPool>() == null) { throw new OperationCanceledException("Flow generator requires an object pool containing flow objects"); }
             if (gestures.GetComponent<GesturesDispatcher>() == null) { throw new OperationCanceledException("Flow generator requires a GestureDispatcher"); }
             flows = flowPool.GetComponent<ObjectPool>();
-            gestures.GetComponent<GesturesDispatcher>().OnSwipeEnd += swipe => GenerateFlow(swipe.Start, swipe.End);
+            gestures.GetComponent<GesturesDispatcher>().OnSwipeEnd += swipe => GenerateFlow(swipe);
         }
 
-        void GenerateFlow(Vector2 start, Vector2 end) {
-            var got = flows.TryRequest(flow => {
-                flow.GetComponent<UnderwaterPhysics.Flow>().Enable(
-                    from: ScreenToWorld(start),
-                    to: ScreenToWorld(end),
+        void GenerateFlow(Swipe swipe) {
+            flows.TryRequest(flow => {
+                var f = flow.GetComponent<Flow>();
+                f.duration = flowDuration;
+                f.strength = maxFlowStrength / (1f + swipe.Duration);
+                f.Enable(
+                    from: ScreenToWorld(swipe.Start),
+                    to: ScreenToWorld(swipe.End),
                     onExausted: () => flow.GetComponent<SharedObject>().ReleaseThis());
             });
         }
