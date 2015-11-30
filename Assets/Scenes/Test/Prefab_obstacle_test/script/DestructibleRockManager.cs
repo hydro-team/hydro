f6b1951a0db5038fe5f7d9a43ea0bde5fa4d00c7
MattiaGianotti <dragonmat1992@live.it>
2015-11-30 17:23:55 +0100

    Hydro animation controller state machine set up.

---
+++ Assets/Scenes/Test/Prefab_obstacle_test/script/DestructibleRockManager.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructibleRockManager : MonoBehaviour {
+
+	public static DestructibleRockManager Instance;
+
+
+	public int currentslice;
+	public GameObject[] rockPool;
+	private bool[,] attached;
+	private float[,] lives;
+	private bool[,] active;
+	public float delay;
+	public Vector3 pit;
+	//public GameObject[] slices;
+	public Vector3[,] positions;
+	private int elementcount;
+	private int numslice;
+
+	void Awake(){
+		Instance = this;
+	}
+
+	public DestructibleRockManager getDestructibleRockManager(){
+		return Instance;
+	}
+
+	// Use this for initialization
+	void Start () {
+		/*slices = new GameObject[transform.childCount];
+		for(int i = 0; i < transform.childCount; i++){
+			slices[i] =transform.GetChild(i).gameObject;
+		}*/
+		numslice = transform.childCount;
+
+		rockPool = GameObject.FindGameObjectsWithTag("destructiblerock");
+		attached = new bool[numslice, rockPool.Length];
+		lives = new float[numslice, rockPool.Length];
+		for(int i = 0; i < numslice; i++){
+			for(int j = 0; j < rockPool.Length; j++){
+				lives[i,j] = 5f;
+			}
+		}
+		active = new bool[numslice, rockPool.Length];
+		Debug.Log (rockPool.Length);
+
+		elementcount = 0;
+		for(int i = 0; i < numslice; i++){
+			int j = transform.GetChild(i).GetChild(1).childCount;
+			if(j > elementcount){
+				elementcount = j;
+			}
+		}
+		positions= new Vector3[numslice,elementcount];
+		for(int i = 0; i < numslice; i++){
+			for(int j = 0; j < elementcount; j++){
+				if(j < transform.GetChild(i).GetChild(1).childCount){
+					positions[i,j] = transform.GetChild(i).GetChild(1).GetChild(j).transform.position;
+				}else{
+					positions[i,j] = pit;
+				}
+			}
+		}
+		Debug.Log (numslice);
+		Debug.Log (elementcount);
+		//Debug.Log(positions[0,0] + "|" + positions[0,1]);
+		//Debug.Log(positions[1,0] + "|" + positions[1,1]);
+
+		relocate(currentslice);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.W) && currentslice < (numslice - 1)){
+			currentslice ++;
+			relocate(currentslice);
+		}
+		if(Input.GetKeyUp(KeyCode.S) && currentslice > 0){
+			currentslice --;
+			relocate(currentslice);
+		}
+	}
+
+	void relocate(int slice){
+		for(int i = 0; i < elementcount; i++){
+			if(i < rockPool.Length ){
+				rockPool[i].SetActive(true);
+				if(rockPool[i].GetComponent<HingeJoint2D>() != null){
+					if(attached[slice,i]){
+						rockPool[i].GetComponent<HingeJoint2D>().enabled = true;
+					}else{
+						rockPool[i].GetComponent<HingeJoint2D>().enabled = false;
+					}
+				}
+				rockPool[i].transform.position = positions[slice, i];
+			}
+			rockPool[i].GetComponent<SmallRock>().setLife(lives[currentslice, i]);
+		}
+		for(int i = elementcount; i < rockPool.Length; i++){
+			if(rockPool[i].GetComponent<HingeJoint2D>() != null){
+				rockPool[i].GetComponent<HingeJoint2D>().enabled = false;
+			}
+			rockPool[i].SetActive(false);
+			rockPool[i].transform.position = pit;
+			rockPool[i].GetComponent<SmallRock>().setLife(lives[currentslice, i]);
+		}
+	}
+
+	public void setAttached(GameObject rock){
+		Debug.Log ("Set Attached called " + currentslice);
+		for(int i = 0; i < rockPool.Length; i++){
+			if(rockPool[i] == rock){
+				attached[currentslice, i] = !attached[currentslice, i];
+			}
+			Debug.Log(attached[currentslice, i]);
+		}
+	}
+
+	public void setIncative(GameObject rock){
+		Debug.Log ("Set Inactive called " + currentslice);
+		for(int i = 0; i < rockPool.Length; i++){
+			if(rockPool[i] == rock){
+				active[currentslice, i] = false;
+				Debug.Log("coroutinestart");
+				StartCoroutine(respawn(i, currentslice, delay));
+				if(rockPool[i].GetComponent<HingeJoint2D>() != null){
+					Destroy(rockPool[i].GetComponent<HingeJoint2D>());
+					setAttached(rockPool[i]);
+				}
+			}
+		}
+		rock.SetActive(false);
+	}
+
+	IEnumerator respawn(int i, int slice, float delayTime)
+	{
+		yield return new WaitForSeconds(delayTime);
+		Debug.Log ("respawned " + i + " " + slice + "life " + lives[slice,i]);
+		lives[slice, i] = 5f;
+		active[slice, i] = true;
+		rockPool[i].GetComponent<SmallRock>().setLife(5);
+		rockPool[i].SetActive(true);
+
+	}
+}
---
+++ Assets/Scenes/Test/Prefab_obstacle_test/script/RockManager.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockManager : MonoBehaviour {
+
+	public static RockManager Instance;
+
+
+	public int currentslice;
+	public GameObject[] rockPool;
+	public bool[,] attached;
+	public Vector3 pit;
+	//public GameObject[] slices;
+	public Vector3[,] positions;
+	private int elementcount;
+	private int numslice;
+
+	void Awake(){
+		Instance = this;
+	}
+
+	public RockManager getRockManager(){
+		return Instance;
+	}
+
+	// Use this for initialization
+	void Start () {
+		/*slices = new GameObject[transform.childCount];
+		for(int i = 0; i < transform.childCount; i++){
+			slices[i] =transform.GetChild(i).gameObject;
+		}*/
+		numslice = transform.childCount;
+
+		rockPool = GameObject.FindGameObjectsWithTag("indestructablerock");
+		attached = new bool[numslice, rockPool.Length];
+		Debug.Log (rockPool.Length);
+
+		elementcount = 0;
+		for(int i = 0; i < numslice; i++){
+			int j = transform.GetChild(i).GetChild(0).childCount;
+			if(j > elementcount){
+				elementcount = j;
+			}
+		}
+		positions= new Vector3[numslice,elementcount];
+		for(int i = 0; i < numslice; i++){
+			for(int j = 0; j < elementcount; j++){
+				if(j < transform.GetChild(i).GetChild(0).childCount){
+					positions[i,j] = transform.GetChild(i).GetChild(0).GetChild(j).transform.position;
+				}else{
+					positions[i,j] = pit;
+				}
+			}
+		}
+		//Debug.Log (numslice);
+		//Debug.Log (elementcount);
+		//Debug.Log(positions[0,0] + "|" + positions[0,1]);
+		//Debug.Log(positions[1,0] + "|" + positions[1,1]);
+
+		relocate(currentslice);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.W) && currentslice < (numslice - 1)){
+			currentslice ++;
+			relocate(currentslice);
+		}
+		if(Input.GetKeyUp(KeyCode.S) && currentslice > 0){
+			currentslice --;
+			relocate(currentslice);
+		}
+	}
+
+	void relocate(int slice){
+		for(int i = 0; i < elementcount; i++){
+			if(i < rockPool.Length ){
+				rockPool[i].SetActive(true);
+				if(rockPool[i].GetComponent<HingeJoint2D>() != null){
+					if(attached[slice,i]){
+						rockPool[i].GetComponent<HingeJoint2D>().enabled = true;
+					}else{
+						rockPool[i].GetComponent<HingeJoint2D>().enabled = false;
+					}
+				}
+				rockPool[i].transform.position = positions[slice, i];
+			}
+		}
+		for(int i = elementcount; i < rockPool.Length; i++){
+			if(rockPool[i].GetComponent<HingeJoint2D>() != null){
+				rockPool[i].GetComponent<HingeJoint2D>().enabled = false;
+			}
+			rockPool[i].SetActive(false);
+			rockPool[i].transform.position = pit;
+		}
+	}
+
+	public void setAttached(GameObject rock){
+		Debug.Log ("Set Attached called " + currentslice);
+		for(int i = 0; i < rockPool.Length; i++){
+			if(rockPool[i] == rock){
+				attached[currentslice, i] = !attached[currentslice, i];
+			}
+			Debug.Log(attached[currentslice, i]);
+		}
+	}
+}
