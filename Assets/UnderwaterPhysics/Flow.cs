44b85921cd8442ecf1d494bd1027c0a98d26d1e8
Francesco Komauli <francesco.komauli@gmail.com>
2015-12-10 18:51:53 +0100

    Flows with particles effect

--- Assets/UnderwaterPhysics/Flow.cs
+++ Assets/UnderwaterPhysics/Flow.cs
@@ -8,8 +8,10 @@ namespace UnderwaterPhysics {
         public float strength;
         public float duration;
 
-        float remainingTime;
+        ParticleSystem bubbles;
         Action onExausted;
+        float remainingTime = 0;
+        bool active = false;
 
         void Awake() {
             if (GetComponent<Collider2D>() == null) {
@@ -17,28 +19,42 @@ namespace UnderwaterPhysics {
             }
             if (strength <= 0) { Debug.LogWarning("Flow: strength should be a positive value, but it is set to " + strength); }
             if (duration <= 0) { Debug.LogWarning("Flow: duration should be a positive value, but it is set to " + duration); }
+            bubbles = GetComponentInChildren<ParticleSystem>();
+            bubbles.Stop();
         }
 
         public void Enable(Vector2 from, Vector2 to, float z = 0, Action onExausted = null) {
-            gameObject.SetActive(true);
             var direction = to - from;
+            var length = direction.magnitude;
             transform.position = (Vector3)((from + to) / 2f) + (Vector3.forward * z);
             transform.rotation = Quaternion.FromToRotation(Vector2.up, direction);
-            transform.localScale = new Vector2(transform.localScale.x, direction.magnitude);
+            transform.localScale = new Vector3(transform.localScale.x, length, 1);
             remainingTime = duration;
             this.onExausted = onExausted;
+            EnableBubbles(length);
+            active = true;
+        }
+
+        void EnableBubbles(float length) {
+            bubbles.transform.localPosition = new Vector3(0, -0.5f);
+            bubbles.startSpeed = strength;
+            bubbles.startLifetime = length / bubbles.startSpeed;
+            bubbles.Play();
         }
 
         void Update() {
+            if (!active) { return; }
             if (remainingTime <= 0) {
+                active = false;
+                bubbles.Stop();
                 if (onExausted != null) { onExausted.Invoke(); }
-                gameObject.SetActive(false);
             } else {
                 remainingTime -= Time.deltaTime;
             }
         }
 
         void OnTriggerStay2D(Collider2D other) {
+            if (!active) { return; }
             var body = other.gameObject.GetComponent<Rigidbody2D>();
             if (body != null) {
                 var force = CalculateForceFor(body);
