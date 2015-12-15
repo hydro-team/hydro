52c67dace941ccacebcf391ce9f61249a8c2a7d4
QbieShay <cislaghi.ilaria@gmail.com>
2015-12-15 10:16:36 +0100

    added the crab

---
+++ Assets/Environment/Crab.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crab : MonoBehaviour {
+	public Crab otherCrab;
+	public Transform Arrival;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider col){
+		if (col.tag == "Player" ) {
+			col.transform.position = Arrival.position;
+		}
+	}
+}
