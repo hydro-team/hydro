64a250236ae5b4911f89b4ec9e2c86c211bd28ae
Francesco Komauli <francesco.komauli@gmail.com>
2015-12-17 09:02:35 +0100

    Points of interest use a trigger area to attract the camera

--- Assets/CameraBehaviour/FollowInterest.cs
+++ Assets/CameraBehaviour/FollowInterest.cs
@@ -1,95 +1,92 @@
 ﻿using UnityEngine;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace CameraBehaviour {
 
+    /// <summary>
+    /// Moves the child camera toward a point of interest when inside of the interest area.
+    /// When not following a point of interest, the camera follows this object.
+    ///
+    /// The approach rate is the proportion of distance covered by the camera toward the point of interest each frame.
+    /// An approach rate of 1 means the immediate placement of the camera over the point of interest.
+    /// Approach rates near to 0 give a smooth transition.
+    ///
+    /// Only points of interest on the same layer of the camera are considered.
+    /// When changing layer, the current point of interest is forget.
+    /// </summary>
     public class FollowInterest : MonoBehaviour {
 
         public WorldManager world;
-        public float captureDistance;
-        public float speedRate;
+        public float approachRate;
 
         new Camera camera;
-        IDictionary<int, IList<PointOfInterest>> pointsOfInterest;
-        Vector2 currentRelativePoint;
         float originalFov;
-        float targetFov;
+        PointOfInterest currentPointOfInterest;
 
         void Awake() {
-            if (speedRate <= 0f || speedRate > 1f) {
-                throw new InvalidOperationException("speedRate out of range ]0,1]: " + speedRate);
+            if (approachRate <= 0f || approachRate > 1f) {
+                throw new InvalidOperationException("approachRate out of range ]0,1]: " + approachRate);
             }
-            camera = GetComponent<Camera>();
+            camera = GetComponentInChildren<Camera>();
             originalFov = camera.fieldOfView;
-            targetFov = originalFov;
-            LookupPointsOfInterestByLayer();
-        }
-
-        void LookupPointsOfInterestByLayer() {
-            pointsOfInterest = FindObjectsOfType<PointOfInterest>()
-                .GroupBy(point => point.gameObject.layer)
-                .ToDictionary(group => group.Key, group => (IList<PointOfInterest>)group.ToList());
         }
 
         void Update() {
-            SearchClosestPointOfInterest();
-            MoveTowardPointOfInterest();
-            Zoom();
+            ForgetPointOfInterestIfOnDifferentLayer();
+            MoveCameraTowardPointOfInterest();
+            AdjustCameraZoom();
         }
 
-        void SearchClosestPointOfInterest() {
-            var layer = world.CurrentSlice.layer;
-            if (!pointsOfInterest.ContainsKey(layer)) {
-                SetDefaultRelativePointAndZoom();
-                return;
-            }
-            var pointOfInterest = pointsOfInterest[layer]
-                .Where(CloseEnough())
-                .OrderBy(point => RelativePosition(point).sqrMagnitude)
-                .FirstOrDefault();
-            if (pointOfInterest == null) {
-                SetDefaultRelativePointAndZoom();
-            } else {
-                currentRelativePoint = RelativePosition(pointOfInterest);
-                targetFov = originalFov + pointOfInterest.fovDifference;
-            }
+        void ForgetPointOfInterestIfOnDifferentLayer() {
+            if (currentPointOfInterest == null) { return; }
+            var layer = currentPointOfInterest.gameObject.layer;
+            if (world.CurrentSlice.layer != layer) { currentPointOfInterest = null; }
         }
 
-        void MoveTowardPointOfInterest() {
-            if ((Vector2)transform.localPosition == currentRelativePoint) { return; }
-            var distance = currentRelativePoint - (Vector2)transform.localPosition;
+        void MoveCameraTowardPointOfInterest() {
+            var displacement = DisplacementOfCurrentPointOfInterest();
+            var cameraPosition = camera.transform.localPosition;
+            if ((Vector2)cameraPosition == displacement) { return; }
+            var distance = displacement - (Vector2)cameraPosition;
             if (distance.sqrMagnitude < 0.0001f) {
-                transform.localPosition = new Vector3(currentRelativePoint.x, currentRelativePoint.y, transform.localPosition.z);
+                camera.transform.localPosition = new Vector3(displacement.x, displacement.y, cameraPosition.z);
                 return;
             }
-            Vector3 movement = (currentRelativePoint - (Vector2)transform.localPosition) * speedRate;
-            transform.localPosition += movement;
+            Vector3 movement = (displacement - (Vector2)cameraPosition) * approachRate;
+            camera.transform.localPosition += movement;
         }
 
-        void Zoom() {
+        Vector2 DisplacementOfCurrentPointOfInterest() {
+            if (currentPointOfInterest == null) { return Vector2.zero; }
+            return currentPointOfInterest.transform.position - transform.position;
+        }
+
+        void AdjustCameraZoom() {
+            var targetFov = TargetFov();
             var fovDifference = targetFov - camera.fieldOfView;
-            if (fovDifference < 0.001) {
+            if (Mathf.Abs(fovDifference) < 0.01f) {
                 camera.fieldOfView = targetFov;
-                return;
+            } else {
+                camera.fieldOfView += fovDifference * approachRate;
             }
-            camera.fieldOfView += fovDifference * speedRate;
         }
 
-        Vector2 RelativePosition(PointOfInterest point) {
-            var position = (Vector2)transform.parent.position;
-            return (Vector2)point.transform.position - position;
+        float TargetFov() {
+            return currentPointOfInterest == null ? originalFov : originalFov + currentPointOfInterest.fovDelta;
         }
 
-        Func<PointOfInterest, bool> CloseEnough() {
-            var sqrCaptureDistance = captureDistance * captureDistance;
-            return point => RelativePosition(point).sqrMagnitude < sqrCaptureDistance;
+        void OnTriggerEnter2D(Collider2D other) {
+            var point = other.gameObject.GetComponent<PointOfInterest>();
+            if (point == null) { return; }
+            if (point.gameObject.layer != world.CurrentSlice.layer) { return; }
+            currentPointOfInterest = point;
         }
 
-        void SetDefaultRelativePointAndZoom() {
-            currentRelativePoint = Vector2.zero;
-            targetFov = originalFov;
+        void OnTriggerExit2D(Collider2D other) {
+            var point = other.gameObject.GetComponent<PointOfInterest>();
+            if (point == null) { return; }
+            if (point != currentPointOfInterest) { return; }
+            currentPointOfInterest = null;
         }
     }
 }
--- Assets/CameraBehaviour/PointOfInterest.cs
+++ Assets/CameraBehaviour/PointOfInterest.cs
@@ -1,9 +1,28 @@
 ﻿using UnityEngine;
+using System;
 
 namespace CameraBehaviour {
 
+    /// <summary>
+    /// Defines a target position and FOV (field of view) for the camera when it enters the zone of this point of interest.
+    /// The target position is given by the position of the game object to which this component is attached.
+    /// The target FOV is the sum of the original FOV of the camera and the specified delta.
+    /// The zone where this point of interest should capture the camera's attention is defined by a collider 2D that must be a trigger.
+    ///
+    /// When setting the FOV delta, consder that the camera should be capable of viewing the entire interest zone.
+    /// Otherwise, the main character could get out of the screen and become unreachable.
+    ///
+    /// Points of interest should be disabled or removed when they become not interesting anymore.
+    /// </summary>
     public class PointOfInterest : MonoBehaviour {
 
-        public float fovDifference = 0;
+        public float fovDelta = 0;
+
+        void Awake() {
+            var collider = GetComponent<Collider2D>();
+            if (collider == null || !collider.isTrigger) {
+                throw new InvalidOperationException("A point of interest must have a trigger collider attached");
+            }
+        }
     }
 }
