4839e1573496d094a06983c5903beb0624660dc0
Francesco Komauli <francesco.komauli@gmail.com>
2015-12-24 15:52:15 +0100

    Main menu music and buttons

---
+++ Assets/Scenes/MainMenu/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+using Sound;
+
+public class MainMenu : MonoBehaviour {
+
+    SoundFacade sounds;
+    Sound.Sound music;
+
+    void Awake() {
+        sounds = GetComponent<SoundFacade>();
+        if (sounds == null) { throw new InvalidOperationException("Missing SoundFacade component"); }
+    }
+
+    void Start() {
+        music = sounds.Play("/ambientali/background");
+    }
+
+    public void LoadTutorial() {
+        music.Stop();
+        Application.LoadLevel("Demomerge");
+    }
+}
