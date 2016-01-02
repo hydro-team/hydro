13e24f1752d1b74bdecbc3638774642d70c64039
QbieShay <cislaghi.ilaria@gmail.com>
2016-01-02 17:48:23 +0100

    Map panel enables and disables on click, working on quest panel

---
+++ Assets/Scenes/Test/GUI/GuiManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiManager : MonoBehaviour {
+
+	public GameObject MapPop;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Map(){
+		MapPop.SetActive (!MapPop.activeSelf);
+	}
+
+	public void Quest(){
+		
+	}
+
+	public void Options(){
+
+	}
+}
