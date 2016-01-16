4c9651fbca8da7b56ecd50e667fe50e6e26d9cd9
MattiaGianotti <dragonmat1992@live.it>
2016-01-16 15:46:22 +0100

    Inventory visible through a bouble

--- Assets/Scenes/Test/Prefab_obstacle_test/Inventory.cs
+++ Assets/Scenes/Test/Prefab_obstacle_test/Inventory.cs
@@ -4,15 +4,34 @@ using System.Collections;
 public class Inventory : MonoBehaviour {
 
 	public Items item;
+	public SpriteRenderer bouble;
+	public SpriteRenderer itemsprite;
+	Color activeboublecolor;
 
 	// Use this for initialization
 	void Start () {
+		activeboublecolor = bouble.color;
 		item = Items.EMPTY;
+		bouble.color = Color.clear;
 	}
 
-	public bool pickUp(Items tag){
+	void Update(){
+		if(item != Items.EMPTY){
+			bouble.color = Color.Lerp(bouble.color, activeboublecolor, Time.deltaTime*2);
+			itemsprite.color = Color.Lerp(itemsprite.color, Color.white, Time.deltaTime*2);
+		}else{
+			bouble.color = Color.Lerp(bouble.color, Color.clear, Time.deltaTime*2);
+			itemsprite.color = Color.Lerp(itemsprite.color, Color.clear, Time.deltaTime*2);
+		}
+		if(itemsprite.color == Color.clear){
+			itemsprite.sprite = null;
+		}
+	}
+
+	public bool pickUp(Items tag, Sprite sprite){
 		if(item == Items.EMPTY){
 			item = tag;
+			itemsprite.sprite = sprite;
 			return true;
 		}else{
 			return false;
