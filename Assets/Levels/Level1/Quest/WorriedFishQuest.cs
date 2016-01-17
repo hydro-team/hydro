931cce16b38a2ec59e125ec60614d18fb1d76e14
QbieShay <cislaghi.ilaria@gmail.com>
2016-01-17 13:28:08 +0100

    Implementing the first level quests

--- Assets/Levels/Level1/Quest/WorriedFishQuest.cs
+++ Assets/Levels/Level1/Quest/WorriedFishQuest.cs
@@ -1,15 +1,102 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using Quests;
+using System;
 
-public class WorriedFishQuest : MonoBehaviour {
+public class WorriedFishQuest : Quest {
 
-	// Use this for initialization
-	void Start () {
-	
+	public override string Name()
+	{
+		return "Lost Friend";
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public override string Description()
+	{
+		return "Find the fish's friend and save him";
+	}
+
+	public override IList<QuestObjective> FirstObjectives(){
+		return new List<QuestObjective> { new FindFriend() };
+	}
+
+	public override void Initialize(QuestsEnvironment environment) {
+		environment.gameObject.AddComponent<Context>();
+	}
+
+	public class Context:MonoBehaviour{
+		public bool fishFoundt;
+		public bool octoFoundt;
+		public bool collectedSeaweed;
+		public bool fedOcto;
+	}
+
+	class FindFriend : QuestObjective{
+		public override string Description()
+		{
+			return "Find the fish's lost friend";
+		}
+
+		public override bool IsOptional(){
+			return false;
+		}
+
+		public override ProgressStatus StatusIn(QuestsEnvironment environment) {
+			return environment.GetComponent<Context>().fishFoundt ? ProgressStatus.SUCCEEDED : ProgressStatus.ONGOING;
+		}
+		public override IList<QuestObjective> NextObjectives() {
+			return new List<QuestObjective> { new FindHelp() };
+		}
+	}
+
+	class FindHelp : QuestObjective{
+		public override string Description(){
+			return "Find someone to move the rocks";
+		}
+
+		public override bool IsOptional(){
+			return false;
+		}
+		public override IList<QuestObjective> NextObjectives() {
+			return new List<QuestObjective> { new FeedOctopus() };
+		}
+		public override ProgressStatus StatusIn(QuestsEnvironment environment) {
+			return environment.GetComponent<Context>().octoFoundt ? ProgressStatus.SUCCEEDED : ProgressStatus.ONGOING;
+		}
+	}
+
+	class FeedOctopus : QuestObjective{
+		public override string Description(){
+			return "The Octopus is weak. Find something he can eat to regain strength";
+		}
+
+		public override bool IsOptional(){
+			return false;
+		}
+		public override IList<QuestObjective> NextObjectives() {
+			//TODO
+			return new List<QuestObjective> { new FeedOctopus() };
+		}
+
+		public override ProgressStatus StatusIn(QuestsEnvironment environment) {
+			return environment.GetComponent<Context>().fedOcto ? ProgressStatus.SUCCEEDED : ProgressStatus.ONGOING;
+		}
+	}
+
+	class GoAndSee : QuestObjective {
+		public override string Description(){
+			return "The Octopus is weak. Find something he can eat to regain strength";
+		}
+
+		public override bool IsOptional(){
+			return false;
+		}
+		public override IList<QuestObjective> NextObjectives() {
+			//TODO
+			return new List<QuestObjective> { new FeedOctopus() };
+		}
+
+		public override ProgressStatus StatusIn(QuestsEnvironment environment) {
+			return environment.GetComponent<Context>().fedOcto ? ProgressStatus.SUCCEEDED : ProgressStatus.ONGOING;
+		}
 	}
 }
