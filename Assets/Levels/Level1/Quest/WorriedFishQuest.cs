efd3088817dbb0642659f96bf444a79f93076803
QbieShay <cislaghi.ilaria@gmail.com>
2016-01-18 10:47:12 +0100

    First fish now working

--- Assets/Levels/Level1/Quest/WorriedFishQuest.cs
+++ Assets/Levels/Level1/Quest/WorriedFishQuest.cs
@@ -26,8 +26,11 @@ public class WorriedFishQuest : Quest {
 	public class Context:MonoBehaviour{
 		public bool fishFoundt;
 		public bool octoFoundt;
+		public bool octoAwake;
 		public bool collectedSeaweed;
 		public bool fedOcto;
+		public bool seeFish;
+		public bool investigate;
 	}
 
 	class FindFriend : QuestObjective{
@@ -57,10 +60,28 @@ public class WorriedFishQuest : Quest {
 			return false;
 		}
 		public override IList<QuestObjective> NextObjectives() {
-			return new List<QuestObjective> { new FeedOctopus() };
+			return new List<QuestObjective> { new WakeOctopus() };
 		}
 		public override ProgressStatus StatusIn(QuestsEnvironment environment) {
 			return environment.GetComponent<Context>().octoFoundt ? ProgressStatus.SUCCEEDED : ProgressStatus.ONGOING;
+		}
+	}
+
+	class WakeOctopus : QuestObjective {
+		public override string Description(){
+			return "This octopus might be helpful. Wake him up";
+		}
+
+		public override bool IsOptional(){
+			return false;
+		}
+		public override IList<QuestObjective> NextObjectives() {
+			//TODO
+			return new List<QuestObjective> { new FeedOctopus() };
+		}
+
+		public override ProgressStatus StatusIn(QuestsEnvironment environment) {
+			return environment.GetComponent<Context>().octoAwake ? ProgressStatus.SUCCEEDED : ProgressStatus.ONGOING;
 		}
 	}
 
@@ -74,7 +95,7 @@ public class WorriedFishQuest : Quest {
 		}
 		public override IList<QuestObjective> NextObjectives() {
 			//TODO
-			return new List<QuestObjective> { new FeedOctopus() };
+			return new List<QuestObjective> { new GoAndSee() };
 		}
 
 		public override ProgressStatus StatusIn(QuestsEnvironment environment) {
@@ -84,7 +105,7 @@ public class WorriedFishQuest : Quest {
 
 	class GoAndSee : QuestObjective {
 		public override string Description(){
-			return "The Octopus is weak. Find something he can eat to regain strength";
+			return "Go back to the trapped fish";
 		}
 
 		public override bool IsOptional(){
@@ -92,11 +113,29 @@ public class WorriedFishQuest : Quest {
 		}
 		public override IList<QuestObjective> NextObjectives() {
 			//TODO
-			return new List<QuestObjective> { new FeedOctopus() };
+			return new List<QuestObjective> { new Investigate() };
 		}
 
 		public override ProgressStatus StatusIn(QuestsEnvironment environment) {
-			return environment.GetComponent<Context>().fedOcto ? ProgressStatus.SUCCEEDED : ProgressStatus.ONGOING;
+			return environment.GetComponent<Context>().seeFish ? ProgressStatus.SUCCEEDED : ProgressStatus.ONGOING;
+		}
+	}
+
+	class Investigate : QuestObjective {
+		public override string Description(){
+			return "This colorful thing seems poisonous. Go and investigate.";
+		}
+
+		public override bool IsOptional(){
+			return false;
+		}
+		public override IList<QuestObjective> NextObjectives() {
+			//TODO
+			return new List<QuestObjective> {};
+		}
+
+		public override ProgressStatus StatusIn(QuestsEnvironment environment) {
+			return environment.GetComponent<Context>().investigate ? ProgressStatus.SUCCEEDED : ProgressStatus.ONGOING;
 		}
 	}
 }
---
+++ Assets/Levels/Level1/Quest/WorriedFishQuestTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using CameraBehaviour;
+using Quests;
+
+public class WorriedFishQuestTrigger : MonoBehaviour {
+
+	public Animator animToSet;
+	public PointOfInterest pinterest;
+	public QuestsEnvironment environment;
+
+
+	void OnTriggerEnter2D ( Collider2D other ) {
+		if (other.tag == "Player") {
+			//TODO Pop bolle con emozioni
+			environment.BeginQuest<WorriedFishQuest.Context> (new WorriedFishQuest ()); 
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other){
+		if (other.tag == "Player") {
+			pinterest.gameObject.SetActive (false);
+			animToSet.SetBool ("roam", true);
+		}
+	}
+
+}
