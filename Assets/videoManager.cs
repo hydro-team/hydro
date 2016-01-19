be0d5a954ef1d3b498826388c37694ef8e7e1b3a
QbieShay <cislaghi.ilaria@gmail.com>
2016-01-19 17:00:37 +0100

    Suppressed videomanager

--- Assets/videoManager.cs
+++ Assets/videoManager.cs
@@ -2,19 +2,19 @@
 using System.Collections;
 
 public class videoManager : MonoBehaviour {
-	public Renderer r ;
-	MovieTexture movie;
-	// Use this for initialization
-	void Start () {
-		r = GetComponent<Renderer>();
-		movie = (MovieTexture)r.material.mainTexture;	
-		movie.Play();
-		StartCoroutine(wait(movie.duration));
-	}
-	
-	IEnumerator wait(float duration) {
-		yield return new WaitForSeconds(duration);
-		movie.Stop();
-		//Application.loadedLevel();
-	}
+//	public Renderer r ;
+//	MovieTexture movie;
+//	// Use this for initialization
+//	void Start () {
+//		r = GetComponent<Renderer>();
+//		movie = (MovieTexture)r.material.mainTexture;	
+//		movie.Play();
+//		StartCoroutine(wait(movie.duration));
+//	}
+//	
+//	IEnumerator wait(float duration) {
+//		yield return new WaitForSeconds(duration);
+//		movie.Stop();
+//		//Application.loadedLevel();
+//	}
 }
