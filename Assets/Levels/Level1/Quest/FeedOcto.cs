0a20f76b7f213508e0458de27265e0fa6ee679fa
MattiaGianotti <dragonmat1992@live.it>
2016-01-19 10:53:37 +0100

    Quest Octopus improved

--- Assets/Levels/Level1/Quest/FeedOcto.cs
+++ Assets/Levels/Level1/Quest/FeedOcto.cs
@@ -6,19 +6,24 @@ public class FeedOcto : MonoBehaviour {
 	
 	public QuestsEnvironment environment;
 	public GameObject targetnextSlice;
+	public Animator anim;
 
 	public void OnTriggerEnter2D(Collider2D collided){
+		Debug.Log(collided.tag);
 		if(collided.gameObject.tag == "Player"){
 			Inventory inv = collided.gameObject.GetComponent<Inventory>();
 			Items seaweed = inv.getInventoy(); 
 			if(seaweed == Items.SEAWEED){
+				Debug.Log("ItemDelivered");
 				inv.freeInventory();
 				environment.GetComponent<WorriedFishQuest.Context>().fedOcto = true;
+				anim.SetBool("MoveTo", true);
 			}
 		}
 	}
 
 	public void endedAniamtion(){
+		transform.position = Vector3.zero;
 		transform.parent = targetnextSlice.transform;
 		transform.position = Vector3.zero;
 		moveToSlice1(transform.GetChild(0).gameObject);
@@ -26,11 +31,13 @@ public class FeedOcto : MonoBehaviour {
 
 	void moveToSlice1(GameObject part){
 		if(part.transform.childCount == 0){
+			part.layer = 12;
 			part.GetComponent<SpriteRenderer>().sortingLayerName = "Slice1";
 		}else{
 			for(int i = 0; i < part.transform.childCount; i++){
 				moveToSlice1(part.transform.GetChild(i).gameObject);
 			}
+			part.layer = 12;
 			part.GetComponent<SpriteRenderer>().sortingLayerName = "Slice1";
 		}
 	}
--- Assets/Levels/Level1/Quest/WakeOcto.cs
+++ Assets/Levels/Level1/Quest/WakeOcto.cs
@@ -6,11 +6,13 @@ public class WakeOcto : MonoBehaviour {
 
 	public QuestsEnvironment environment;
 	public Animator OctoAnimator;
+	public GameObject particle;
 
 	void OnTriggerEnter2D (Collider2D other){
 		if (other.gameObject.tag == "Flow") {
 			environment.GetComponent<WorriedFishQuest.Context>().octoAwake = true;
 			OctoAnimator.SetBool("Awake", true);
+			particle.SetActive(false);
 		}
 	}
 }
