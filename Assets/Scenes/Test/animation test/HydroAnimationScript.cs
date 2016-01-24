3eea01cfb236a61570a0117784aafb17d8f9d45e
Francesco Komauli <francesco.komauli@gmail.com>
2016-01-24 17:22:38 +0100

    Hydro animation script preserves initial scale

--- Assets/Scenes/Test/animation test/HydroAnimationScript.cs
+++ Assets/Scenes/Test/animation test/HydroAnimationScript.cs
@@ -4,85 +4,86 @@ using System.Collections;
 
 public class HydroAnimationScript : MonoBehaviour {
 
-    private static readonly Vector3 DEFAULT_SCALE = new Vector3(1f, 1f, 1f);
-    private static readonly Vector3 FLIPPED_SCALE = new Vector3(-1f, 1f, 1f);
-
     public Animator animHydroSprite;
-	public Animator animCamera;
-	public Animator animSliceHydro;
-	public GameObject cam;
-	public float threshold;
+    public Animator animCamera;
+    public Animator animSliceHydro;
+    public GameObject cam;
+    public float threshold;
 
-	public SpriteRenderer[] sprites;
+    public SpriteRenderer[] sprites;
 
-	private Rigidbody2D rigidbody;
+    private Rigidbody2D body;
 
-	void Start () {
-		animHydroSprite.SetFloat("Speed",0f);
-		rigidbody = gameObject.GetComponent<Rigidbody2D> ();
+    void Start() {
+        animHydroSprite.SetFloat("Speed", 0f);
+        body = gameObject.GetComponent<Rigidbody2D>();
 
-	}
+    }
 
-	void Update () {
+    void Update() {
         LookTowardMovementDirection();
         SetMotionState();
-	}
+    }
 
     void LookTowardMovementDirection() {
-        if (rigidbody.velocity.x > threshold) {
-            transform.localScale = FLIPPED_SCALE;
-            animCamera.transform.parent.localScale = FLIPPED_SCALE;
-        } else if (rigidbody.velocity.x < -threshold) {
-            transform.localScale = DEFAULT_SCALE;
-            animCamera.transform.parent.localScale = DEFAULT_SCALE;
+        var scale = transform.localScale;
+        var cameraScale = animCamera.transform.parent.localScale;
+        if (body.velocity.x > threshold) {
+            scale.x = -Mathf.Abs(scale.x);
+            cameraScale.x = -Mathf.Abs(cameraScale.x);
+            transform.localScale = scale;
+            animCamera.transform.parent.localScale = cameraScale;
+        } else if (body.velocity.x < -threshold) {
+            scale.x = Mathf.Abs(scale.x);
+            cameraScale.x = Mathf.Abs(cameraScale.x);
+            transform.localScale = scale;
+            animCamera.transform.parent.localScale = cameraScale;
         }
     }
 
-	public void CreateCurrent(){
-		animHydroSprite.SetTrigger ("Current");
-	}
+    public void CreateCurrent() {
+        animHydroSprite.SetTrigger("Current");
+    }
 
     void SetMotionState() {
-        var moving = rigidbody.velocity.sqrMagnitude > threshold;
+        var moving = body.velocity.sqrMagnitude > threshold;
         animHydroSprite.SetBool("Moving", moving);
     }
 
-	public void animCameraFarNear(bool far_near){
-		animCamera.SetBool("Far_near", far_near);
-		animCamera.SetTrigger("MoveCamera");
-	}
+    public void animCameraFarNear(bool far_near) {
+        animCamera.SetBool("Far_near", far_near);
+        animCamera.SetTrigger("MoveCamera");
+    }
 
-	public void animCameraConfirm(){
-		animCamera.SetTrigger ("Confirmed");
-	}
-	public void animCameraCancel(){
-		animCamera.SetTrigger("Canceled");
-	}
+    public void animCameraConfirm() {
+        animCamera.SetTrigger("Confirmed");
+    }
+    public void animCameraCancel() {
+        animCamera.SetTrigger("Canceled");
+    }
 
-	public void animHydroFarNear(bool far_near){
+    public void animHydroFarNear(bool far_near) {
 
-		if(far_near){
-			animHydroSprite.SetTrigger("SliceIN");
-		}else{
-			animHydroSprite.SetTrigger("SliceOUT");
-		}
-	}
+        if (far_near) {
+            animHydroSprite.SetTrigger("SliceIN");
+        } else {
+            animHydroSprite.SetTrigger("SliceOUT");
+        }
+    }
 
-	public void animBounce(bool far_near){
-		animHydroSprite.SetBool("Far_near", far_near);
-		animHydroSprite.SetTrigger("Bounce");
-		animCameraCancel();
-	}
+    public void animBounce(bool far_near) {
+        animHydroSprite.SetBool("Far_near", far_near);
+        animHydroSprite.SetTrigger("Bounce");
+        animCameraCancel();
+    }
 
-	public void animMove(){
-		animHydroSprite.SetTrigger("Move");
-	}
+    public void animMove() {
+        animHydroSprite.SetTrigger("Move");
+    }
 
-	public void switchSlice(GameObject slice){
-		foreach (SpriteRenderer spr in sprites) {
-			spr.sortingLayerName = LayerMask.LayerToName(slice.layer);
-			Debug.Log("Switched"
-			          );
-		}
-	}
+    public void switchSlice(GameObject slice) {
+        foreach (SpriteRenderer spr in sprites) {
+            spr.sortingLayerName = LayerMask.LayerToName(slice.layer);
+        }
+    }
 }
