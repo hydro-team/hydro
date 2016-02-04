f1814f56069bfd9ba6dda42b828572c947f87f5a
Francesco Komauli <francesco.komauli@gmail.com>
2016-02-04 18:53:45 +0100

    Embedded skippable video scene before the first level

--- Assets/Resources/VideoPlayerMio.cs
+++ Assets/Resources/VideoPlayerMio.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class VideoPlayerMio : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		((MovieTexture)GetComponent<Renderer> ().material.mainTexture).Play ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public string nextScene = "ui";
+
+    MovieTexture movie;
+
+    void Awake() {
+        movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+        movie.loop = false;
+    }
+
+    void Start() {
+        movie.Play();
+    }
+
+    void Update() {
+        if (Input.anyKey) { movie.Stop(); }
+        if (!movie.isPlaying) { Application.LoadLevel(nextScene); }
+    }
 }
